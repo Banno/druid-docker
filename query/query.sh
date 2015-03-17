75b993e686bb823ae0df7c298784fe67da4d4bc6
Zach Cox <zach.cox@banno.com>
2015-03-17 12:46:08 -0500

    run 2 overlords

--- query/query.sh
+++ query/query.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
+#when using random port chosen by marathon, need to check broker logs (or Zookeeper) to get the right host and port to use
 BROKER_HOST=mesosslave1.dev.banno.com
-BROKER_PORT=31821
+BROKER_PORT=31433
+
 JSON_FILE=$1
 curl -X POST "http://${BROKER_HOST}:${BROKER_PORT}/druid/v2/?pretty" -H 'content-type: application/json'  -d @$JSON_FILE
