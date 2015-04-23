962794d670e488a8dab92f4cced7bee570f7c0d1
Zach Cox <zach.cox@banno.com>
2015-04-23 13:01:29 -0500

    local build notes

--- base/pull-deps.sh
+++ base/pull-deps.sh
@@ -4,5 +4,6 @@ EXTENSIONS=$1
 DRUID_PROPERTIES_FILE=/opt/druid/config/_common/common.runtime.properties
 echo "druid.extensions.coordinates=$EXTENSIONS" >> $DRUID_PROPERTIES_FILE
 echo "druid.extensions.localRepository=/opt/druid/repository" >> $DRUID_PROPERTIES_FILE
+#echo 'druid.extensions.remoteRepositories=["http://192.168.59.3:9090", "http://repo1.maven.org/maven2/", "https://metamx.artifactoryonline.com/metamx/pub-libs-releases-local"]' >> $DRUID_PROPERTIES_FILE
 
 java -cp "/opt/druid/config/_common:/opt/druid/lib/*" io.druid.cli.Main tools pull-deps
