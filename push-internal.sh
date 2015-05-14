96568e16ef0c15bb99436bf12e00306e215094da
Zach Cox <zach.cox@banno.com>
2015-05-14 09:12:56 -0500

    nginx registry workaround

--- push-internal.sh
+++ push-internal.sh
@@ -3,8 +3,8 @@
 TAG="latest"
 
 docker push registry.banno-internal.com/druid-base:$TAG
-docker push registry.banno-internal.com/druid-hadoop-base:$TAG
+docker push registry.banno-internal.com:7000/druid-hadoop-base:$TAG
 docker push registry.banno-internal.com/druid-broker:$TAG
 docker push registry.banno-internal.com/druid-coordinator:$TAG
 docker push registry.banno-internal.com/druid-historical:$TAG
-docker push registry.banno-internal.com/druid-overlord:$TAG
+docker push registry.banno-internal.com:7000/druid-overlord:$TAG
