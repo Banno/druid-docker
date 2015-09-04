2179efb149637c1cc3e4bb73d5666d3c4ada5202
jwinder <jsph.winder@gmail.com>
2015-09-04 13:55:40 -0500

    Druid realtime base image.

--- build-internal.sh
+++ build-internal.sh
@@ -7,4 +7,5 @@ docker build -t registry.banno-internal.com/druid-hadoop-base:$TAG hadoop-base
 docker build -t registry.banno-internal.com/druid-broker:$TAG broker
 docker build -t registry.banno-internal.com/druid-coordinator:$TAG coordinator
 docker build -t registry.banno-internal.com/druid-historical:$TAG historical
+docker build -t registry.banno-internal.com/druid-realtime:$TAG realtime
 docker build -t registry.banno-internal.com/druid-overlord:$TAG overlord
--- push-internal.sh
+++ push-internal.sh
@@ -7,4 +7,5 @@ docker push registry.banno-internal.com/druid-hadoop-base:$TAG
 docker push registry.banno-internal.com/druid-broker:$TAG
 docker push registry.banno-internal.com/druid-coordinator:$TAG
 docker push registry.banno-internal.com/druid-historical:$TAG
+docker push registry.banno-internal.com/druid-realtime:$TAG
 docker push registry.banno-internal.com/druid-overlord:$TAG
